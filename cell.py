853446d3d6591911e2f4cae19fbb94d3d26be798
cmsato09 <chrismsato@gmail.com>
2022-06-30 20:28:25 -0700

    fix: roll back to previous commit since main() no longer ran
    - added comments on each cell class function

--- cell.py
+++ cell.py
@@ -7,10 +7,10 @@ import sys
 
 class Cell:
     all_cells = []
-    cell_count = settings.CELL_COUNT - settings.MINE_NUMBER
+    cell_count = settings.CELL_COUNT - settings.MINE_NUMBER # could use len(all_cells)?
     cell_count_label_object = None
     mine_count = settings.MINE_NUMBER
-    text_color = {
+    text_color = { # used in show_cell function when displaying text color
         0: "white",
         1: "blue",
         2: "green",
@@ -36,6 +36,11 @@ class Cell:
         return f"Cell({self.x}, {self.y})"
 
     def create_button_obj(self, location):
+        """
+        Creates a button object for each cell which is then populated in
+        'location' frame.
+        Button set to self.cell_button_obj
+        """
         btn = Button(
             location,  # place button in a Frame()
             width=2,
@@ -58,7 +63,12 @@ class Cell:
         Cell.cell_count_label_object = label
 
     def left_click_action(self, event):
-        """When button is left-clicked, """
+        """
+        When button is left-clicked on a cell button, it will open the cell and
+        show how many mines are surrounding the cell. If cell is a mine, you
+        lose. If you successfully click all the cells that aren't mines, you
+        win!
+        """
         # tkinter convention to have one more parameter to assign something to an event
         if self.is_mine:
             self.show_mine()
@@ -70,13 +80,13 @@ class Cell:
             if Cell.cell_count == settings.MINE_NUMBER:
                 ctypes.windll.user32.MessageBoxW(0, 'YOU WIN', 'WIN', 0)
 
-
-
     def right_click_action(self, event):
-        """ When button is right-clicked, the button is flagged as a possible
-            mine. The background of the button switches to yellow and flag
-            count is decreased by 1. When button is already flagged, it reverts
-            to the default color and flag count is increased by 1."""
+        """
+        When button is right-clicked, the button is flagged as a possible mine.
+        The background of the button switches to yellow and flag count is
+        decreased by 1. When button is already flagged, the button reverts to
+        the default color and flag count is increased by 1.
+        """
 
         if not self.is_mine_candidate:
             self.cell_button_obj.configure(
@@ -85,11 +95,12 @@ class Cell:
             self.is_mine_candidate = True
             Cell.mine_count -= 1
 
-            if Cell.cell_count_label_object:
-                Cell.cell_count_label_object.configure(
-                    text=f"Cells Left: {Cell.cell_count}\n"
-                         f"Flags Left: {Cell.mine_count}",
-                )
+            self.update_count()
+            # if Cell.cell_count_label_object:
+            #     Cell.cell_count_label_object.configure(
+            #         text=f"Cells Left: {Cell.cell_count}\n"
+            #              f"Flags Left: {Cell.mine_count}",
+            #     )
         else:
             self.cell_button_obj.configure(
                 bg='SystemButtonFace',
@@ -97,21 +108,30 @@ class Cell:
             self.is_mine_candidate = False
             Cell.mine_count += 1
 
-            #TODO make new function for updating cell and mine count
-            if Cell.cell_count_label_object:
-                Cell.cell_count_label_object.configure(
-                    text=f"Cells Left: {Cell.cell_count}\n"
-                         f"Flags Left: {Cell.mine_count}",
-                )
+            self.update_count()
+            #make new function for updating cell and mine count
+            # if Cell.cell_count_label_object:
+            #     Cell.cell_count_label_object.configure(
+            #         text=f"Cells Left: {Cell.cell_count}\n"
+            #              f"Flags Left: {Cell.mine_count}",
+            #     )
 
     def get_cell(self, x, y):
-        # Return cell object based on x, y grid coordinates
+        """
+        Return cell object from all_cells list given x and y grid coordinates.
+        Used in surrounding_cells function
+        """
         for cell in Cell.all_cells:
             if cell.x == x and cell.y == y:
                 return cell
 
     @property
     def surrounding_cells(self):
+        """
+        Returns the coordinates of surrounding cells. If coordinates outside
+        of grid coordinates (aka doesn't exist), it is defined as None and not
+        a part of the list (e.g. corner cells only have 3 surrounding cells)
+        """
         cells_around_clicked = [
             self.get_cell(self.x - 1, self.y - 1),
             self.get_cell(self.x - 1, self.y),
@@ -128,6 +148,10 @@ class Cell:
 
     @property
     def surrounding_cells_mine_number(self):
+        """
+        Counts number of mine cells in surrounding_cells list.
+        Used in show_cell function
+        """
         counter = 0
         for cell in self.surrounding_cells:
             if cell.is_mine:
@@ -135,18 +159,24 @@ class Cell:
         return counter
 
     def show_cell(self):
-        """ Displays number of mines surrounding cell when cell is opened. """
+        """
+        Displays number of mines surrounding cell when cell is opened.
+        Text color depends on number.
+        Unbinds button once cell is 'opened' so you can't flag it.
+        """
         if not self.is_opened:
             Cell.cell_count -= 1
             self.cell_button_obj.configure(
                 text=f"{self.surrounding_cells_mine_number}",
                 fg=f"{Cell.text_color[self.surrounding_cells_mine_number]}",
             )
-            if Cell.cell_count_label_object:
-                Cell.cell_count_label_object.configure(
-                    text=f"Cells Left: {Cell.cell_count}\n"
-                         f"Flags Left: {Cell.mine_count}",
-                )
+
+            self.update_count()
+            # if Cell.cell_count_label_object:
+            #     Cell.cell_count_label_object.configure(
+            #         text=f"Cells Left: {Cell.cell_count}\n"
+            #              f"Flags Left: {Cell.mine_count}",
+            #     )
             # If mine candidate, we should still be able to click it and the
             # button configures back to SystemButtonFace
             self.cell_button_obj.configure(bg='SystemButtonFace')
@@ -156,8 +186,10 @@ class Cell:
         self.cell_button_obj.unbind('<Button-3>')
 
     def show_surrounding_cells(self):
-        """ Opens surrounding cells if cell with zero surrounding mines is
-            opened """
+        """
+        Opens surrounding cells if cell with zero surrounding mines is
+        opened.
+        """
         if self.surrounding_cells_mine_number == 0:
             for cell_obj in self.surrounding_cells:
                 if not cell_obj.is_opened:
@@ -166,16 +198,31 @@ class Cell:
                     cell_obj.show_surrounding_cells()
 
     def show_mine(self):
-        """ When a mine button is shown, the background turns red."""
+        """
+        When a mine button is shown, the background of the cell turns red.
+        Hit a mine, game over.
+        """
         self.cell_button_obj.configure(bg="red")
         ctypes.windll.user32.MessageBoxW(0, 'MINE, BOOM', 'GAME OVER', 0)
         # sys.exit()  # exits game, TODO restart the game when you lose
 
     @staticmethod
     def randomize_mines():
-        """ Randomly selects cells to become mines and number of mines depends
-            on grid size and mine number """
-
+        """
+        Randomly selects cells to become mines in all_cells list.
+        Number of mines depends on grid size and mine number settings.
+        """
         selected_cells = random.sample(Cell.all_cells, settings.MINE_NUMBER)
         for picked_cell in selected_cells:
             picked_cell.is_mine = True
+
+    @staticmethod
+    def update_count():
+        """
+        Updates cell and flag count labels in top_frame
+        """
+        #if Cell.cell_count_label_object:
+        Cell.cell_count_label_object.configure(
+            text=f"Cells Left: {Cell.cell_count}\n"
+                 f"Flags Left: {Cell.mine_count}",
+        )
